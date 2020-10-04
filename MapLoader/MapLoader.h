0e3e357b43199e775d80add92745dc099e8fbd95
fs <farah.salhany@hotmail.com>
2020-10-04 13:04:31 -0400

    First commit

---
+++ MapLoader/MapLoader.h
@@ -0,0 +1,11 @@
+//
+// The Map loader should:
+// 1- read and load any map file
+// 2- Store the map as a graph data structure (Part 1)
+// 3- read any text file (even the ones that do not constitute a valid map)
+//--------------------
+// All data members of user-defined class type must be of pointer type
+// All classes must implement a correct copy constructor, assignment operator,
+// and stream insertion operator
+// memory leaks must be avoided
+//
---
+++ MapLoader/MapLoaderDriver.cpp
@@ -0,0 +1,5 @@
+//
+// Map loader driver must reads various files and successfully creates a map object for
+// a valid map files, and rejects invalid map files of different kinds.
+//
+
