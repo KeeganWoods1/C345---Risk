0cef9c3d6b0f7c4213f75e6997f41abb076565ff
jakelam75000 <46825425+jakelam75000@users.noreply.github.com>
2020-10-07 16:02:21 -0400

    added fiels via upload fully fixed cpp

--- deliverable 1 orderlist/Order.cpp
+++ deliverable 1 orderlist/Order.cpp
@@ -13,9 +13,9 @@ using namespace std;
 	}
 	Order::~Order() { isnull = true; };
 	Order::Order(const Order&) {};
-	Order& Order::operator = (const Order & o) {};/*
+	Order& Order::operator = (const Order & o) {};
 	istream& operator >> (istream& stream, Order& o) {};
-	ostream& operator << (ostream& out, const Order& o) {};*/
+	ostream& operator << (ostream& out, const Order& o) {};
 	bool Order::getisnull() {
 		return isnull;
 	}
@@ -54,61 +54,157 @@ using namespace std;
 	}
 	Advanceorder& Advanceorder::operator = (const Advanceorder& o) {
 
-	}/*
-	friend istream& operator >> (istream& stream, Advanceorder& o) {
+	}
+	istream& operator >> (istream& stream, Advanceorder& o) {
 
 	}
-	friend ostream& operator << (ostream& out, const Advanceorder& o) {
+	ostream& operator << (ostream& out, const Advanceorder& o) {
 
-	}*/
+	}
 
 	//will need to add the other pointers to the consturctor and destructor
-	airliftorder::airliftorder(int i, Territory sourceterritory, Territory destinationterritory, Player orderplayer);
-	bool airliftorder::validate();
-	bool airliftorder::execute();
-	airliftorder::~airliftorder();
-	airliftorder::airliftorder(const airliftorder&);
-	airliftorder& airliftorder::operator = (const airliftorder& o);/*
-	friend istream& operator >> (istream& stream, airliftorder& o);
-	friend ostream& operator << (ostream& out, const airliftorder& o);*/
-	Blockadeorder::Blockadeorder(Player orderp, Territory destt);
-	bool Blockadeorder::validate();
-	bool Blockadeorder::execute();
-	Blockadeorder::~Blockadeorder();
-	Blockadeorder::Blockadeorder(const Blockadeorder&);
-	Blockadeorder::Blockadeorder& operator = (const Blockadeorder& o);/*
-	friend istream& operator >> (istream& stream, Blockadeorder& o);
-	friend ostream& operator << (ostream& out, const Blockadeorder& o);*/
-	Bomborder::Bomborder(Player orderp, Territory destt);
-	bool Bomborder::validate();
-	bool Bomborder::execute();
-	Bomborder::~Bomborder();
-	Bomborder::Bomborder(const Bomborder&);
-	Bomborder::Bomborder& operator = (const Bomborder& o);/*
-	friend istream& operator >> (istream& stream, Bomborder& o);
-	friend ostream& operator << (ostream& out, const Bomborder& o);*/
-	Deployorder::Deployorder(Player orderp, int j, Territory destinationterritory);
-	bool Deployorder::validate();
-	bool Deployorder::execute();
-	Deployorder::~Deployorder();
-	Deployorder::Deployorder(const Deployorder&);
-	Deployorder::Deployorder& operator = (const Deployorder& o);/*
-	friend istream& operator >> (istream& stream, Deployorder& o);
-	friend ostream& operator << (ostream& out, const Deployorder& o);*/
-	Negotiateorder::Negotiateorder(Player orderp, Player destp);
-	bool Negotiateorder::validate();
-	bool Negotiateorder::execute();
-	Negotiateorder::~Negotiateorder();
-	Negotiateorder::Negotiateorder(const Negotiateorder&);
-	Negotiateorder::Negotiateorder& operator = (const Negotiateorder& o);/*
-	friend istream& operator >> (istream& stream, Negotiateorder& o);
-	friend ostream& operator << (ostream& out, const Negotiateorder& o);*/
+	airliftorder::airliftorder(int i, Territory sourceterritory, Territory destinationterritory, Player orderplayer) {
 
-	Reinforcementorder::Reinforcementorder(Player orderp);
-	bool Reinforcementorder::validate();
-	bool Reinforcementorder::execute();
-	Reinforcementorder::~Reinforcementorder();
-	Reinforcementorder::Reinforcementorder(const Reinforcementorder&);
-	Reinforcementorder& Reinforcementorder::operator = (const Reinforcementorder& o);/*
-	friend istream& operator >> (istream& stream, Reinforcementorder& o);
-	friend ostream& operator << (ostream& out, const Reinforcementorder& o);*/
+	}
+	bool airliftorder::validate() {
+
+	}
+	bool airliftorder::execute() {
+
+	}
+	airliftorder::~airliftorder() {
+
+	}
+	airliftorder::airliftorder(const airliftorder&) {
+
+	}
+	airliftorder& airliftorder::operator = (const airliftorder& o) {
+
+	}
+	istream& operator >> (istream& stream, airliftorder& o) {
+
+	}
+	ostream& operator << (ostream& out, const airliftorder& o) {
+
+	}
+	Blockadeorder::Blockadeorder(Player orderp, Territory destt) {
+
+	}
+	bool Blockadeorder::validate() {
+
+	}
+	bool Blockadeorder::execute() {
+
+	}
+	Blockadeorder::~Blockadeorder() {
+
+	}
+	Blockadeorder::Blockadeorder(const Blockadeorder&) {
+
+	}
+	Blockadeorder& Blockadeorder::operator = (const Blockadeorder& o) {
+
+	}
+	istream& operator >> (istream& stream, Blockadeorder& o) {
+
+	}
+	ostream& operator << (ostream& out, const Blockadeorder& o) {
+
+	}
+	Bomborder::Bomborder(Player orderp, Territory destt) {
+
+	}
+	bool Bomborder::validate() {
+
+	}
+	bool Bomborder::execute() {
+
+	}
+	Bomborder::~Bomborder() {
+
+	}
+	Bomborder::Bomborder(const Bomborder&) {
+
+	}
+	Bomborder& Bomborder::operator = (const Bomborder& o) {
+
+	}
+	istream& operator >> (istream& stream, Bomborder& o) {
+
+	}
+	ostream& operator << (ostream& out, const Bomborder& o) {
+
+	}
+	Deployorder::Deployorder(Player orderp, int j, Territory destinationterritory) {
+
+	}
+	bool Deployorder::validate() {
+
+	}
+	bool Deployorder::execute() {
+
+	}
+	Deployorder::~Deployorder() {
+
+	}
+	Deployorder::Deployorder(const Deployorder&) {
+
+	}
+	Deployorder& Deployorder::operator = (const Deployorder& o) {
+
+	}
+	istream& operator >> (istream& stream, Deployorder& o) {
+
+	}
+	ostream& operator << (ostream& out, const Deployorder& o) {
+
+	}
+	Negotiateorder::Negotiateorder(Player orderp, Player destp) {
+
+	}
+	bool Negotiateorder::validate() {
+
+	}
+	bool Negotiateorder::execute() {
+
+	}
+	Negotiateorder::~Negotiateorder() {
+
+	}
+	Negotiateorder::Negotiateorder(const Negotiateorder&) {
+
+	}
+	Negotiateorder& Negotiateorder::operator = (const Negotiateorder& o) {
+
+	}
+	istream& operator >> (istream& stream, Negotiateorder& o) {
+
+	}
+	ostream& operator << (ostream& out, const Negotiateorder& o) {
+
+	}
+
+	Reinforcementorder::Reinforcementorder(Player orderp) {
+
+	}
+	bool Reinforcementorder::validate() {
+
+	}
+	bool Reinforcementorder::execute() {
+
+	}
+	Reinforcementorder::~Reinforcementorder() {
+
+	}
+	Reinforcementorder::Reinforcementorder(const Reinforcementorder&) {
+
+	}
+	Reinforcementorder& Reinforcementorder::operator = (const Reinforcementorder& o) {
+
+	}
+	istream& operator >> (istream& stream, Reinforcementorder& o) {
+
+	}
+	ostream& operator << (ostream& out, const Reinforcementorder& o) {
+
+	}
