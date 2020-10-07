e4edeb939b173c6a1f8e4c729164a474ed22a444
salehatrq <68137308+salehatrq@users.noreply.github.com>
2020-10-07 02:27:09 -0400

    Update PlayerDriver.cpp

--- Player/PlayerDriver.cpp
+++ Player/PlayerDriver.cpp
@@ -7,12 +7,22 @@ using namespace std;
 #include "Map.cpp"
 
 int main() {
+    
+    // setting player's name
+    Player a;
+    cout << "Please enter your name: ";
+    string input;
+    cin >> input;
+    a.setName(input);
+    cout << "Hello, " << a.getName() << endl;
 
+    // testing toDefend method
     Territory c;
     for(int i =0; i < 2; i++){
         cout<< c.toDefend().at(i)->territory_name <<endl;
     }
 
+    // testing to Attack method
     for(int i =0; i < 2; i++){
         cout<< c.toAttack().at(i)->territory_name <<endl;
     }
