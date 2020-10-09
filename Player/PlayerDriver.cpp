5f3923020469680a424a87f26665ef9cfe72f527
salehatrq <68137308+salehatrq@users.noreply.github.com>
2020-10-09 02:39:57 -0400

    Update PlayerDriver.cpp

--- Player/PlayerDriver.cpp
+++ Player/PlayerDriver.cpp
@@ -1,29 +1,38 @@
 #include <iostream> 
-using namespace std; 
-
-#include "Player.h"
 #include "Player.cpp"
-#include "Map.h"
 #include "Map.cpp"
+#include "Order.cpp"
+#include "Orderlist.cpp"
+#include "Card.cpp"
+using namespace std; 
 
 int main() {
     
     // setting player's name
-    Player a;
+    Player* a = new Player();
     cout << "Please enter your name: ";
     string input;
     cin >> input;
-    a.setName(input);
-    cout << "Hello, " << a.getName() << endl;
+    a->setName(input);
+    cout << "Hello, player " << a->getName() << endl;
+   
+    // demonstrating the player owns a collection of territories
+    cout << "The current number of territories owned by " << a->getName() << ": " << a->getNumTerrOwned() << endl;
+
 
     // testing toDefend method
     Territory c;
+    cout << "Territories to be defended: " << endl;
     for(int i =0; i < 2; i++){
-        cout<< c.toDefend().at(i)->territory_name <<endl;
+        cout<< c.toDefend().at(i)->getTerritoryName() << endl;
     }
 
     // testing to Attack method
+    cout<< "Territories to be attacked: " << endl;
     for(int i =0; i < 2; i++){
-        cout<< c.toAttack().at(i)->territory_name <<endl;
+        cout<< c.toAttack().at(i)->getTerritoryName() << endl;
     }
+
+    // testing issueOrder method
+    
 }
