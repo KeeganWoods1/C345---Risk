120d2f3a1f3e58ac7f25d3d19fe59256b0523013
salehatrq <68137308+salehatrq@users.noreply.github.com>
2020-10-11 01:25:41 -0400

    Update PlayerDriver.cpp

--- Player/PlayerDriver.cpp
+++ Player/PlayerDriver.cpp
@@ -1,37 +1,48 @@
 #include <iostream> 
+#include <vector>
 #include "Player.cpp"
 #include "Map.cpp"
 #include "Order.cpp"
 #include "Orderlist.cpp"
-#include "Card.cpp"
+#include "Cards.cpp"
+
 using namespace std; 
 
 int main() {
     
-    // setting player's name
-    Player* a = new Player();
-    cout << "Please enter your name: ";
-    string input;
-    cin >> input;
-    a->setName(input);
-    cout << "Hello, player " << a->getName() << endl;
-   
+    // creating a new player with a name
+    Player* a = new Player("Saleha");
+    
     // demonstrating the player owns a collection of territories
-    cout << "The current number of territories owned by " << a->getName() << ": " << a->getNumTerrOwned() << endl;
+    cout << "The current number of territories owned by " << a->getName() << ": " << a->getNumTerrOwned() << endl<<endl;
 
+    // demonstrating the player owns a hand of cards
+    // creating a deck and drawing 5 cards for the player to have a hand of 3 cards
+    Deck* deckOfCards = new Deck();
+    // 1st draw
+    deckOfCards->draw(a->getHand(), deckOfCards);
+    cout << "The cards owned by " << a->getName() << " after the 1st draw: " << *(a->getHand());
+    // 2nd draw
+    deckOfCards->draw(a->getHand(), deckOfCards);
+    // 3rd draw
+    deckOfCards->draw(a->getHand(), deckOfCards);
+    cout << endl << "The cards owned by " << a->getName() << " after the 3rd draw: " << endl << *(a->getHand()) << endl;
+    
     // testing toDefend method
-    Territory c;
     cout << "Territories to be defended: " << endl;
     for(int i =0; i < 2; i++){
-        cout<< c.toDefend().at(i)->getTerritoryName() << endl;
+        cout<< a->toDefend().at(i)->getTerritoryName() << endl;
     }
 
-    // testing to Attack method
+    // testing toAttack method
     cout<< "Territories to be attacked: " << endl;
     for(int i =0; i < 2; i++){
-        cout<< c.toAttack().at(i)->getTerritoryName() << endl;
+        cout<< a->toAttack().at(i)->getTerritoryName() << endl;
     }
 
     // testing issueOrder method
+    a->issueOrder();
     
+    //cout << "Player's orderlist contains orders " << a->getPlayerolist();
+
 }
