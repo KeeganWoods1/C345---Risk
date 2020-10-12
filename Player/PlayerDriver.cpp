a4d7a9fd44f54fc3fd1507f4136a46aea27d0f67
salehatrq <68137308+salehatrq@users.noreply.github.com>
2020-10-12 18:10:25 -0400

    Update PlayerDriver.cpp

--- Player/PlayerDriver.cpp
+++ Player/PlayerDriver.cpp
@@ -11,13 +11,14 @@ using namespace std;
 int main() {
     
     // creating a new player with a name
-    Player* a = new Player("Saleha");
+    Player* a = new Player("John");
+    cout << endl << "The current number of players in the game: " << a->getPlayerCount() <<endl;
     
     // demonstrating the player owns a collection of territories
     cout << "The current number of territories owned by " << a->getName() << ": " << a->getNumTerrOwned() << endl<<endl;
 
     // demonstrating the player owns a hand of cards
-    // creating a deck and drawing 5 cards for the player to have a hand of 3 cards
+    // creating a deck and drawing cards for the player to have a hand of 3 cards
     Deck* deckOfCards = new Deck();
     // 1st draw
     deckOfCards->draw(a->getHand(), deckOfCards);
@@ -30,17 +31,24 @@ int main() {
     
     // testing toDefend method
     cout << "Territories to be defended: " << endl;
-    for(int i =0; i < 2; i++){
+    for(int i =0; i < 4; i++){
         cout<< a->toDefend().at(i)->getTerritoryName() << endl;
     }
 
     // testing toAttack method
     cout<< "Territories to be attacked: " << endl;
-    for(int i =0; i < 2; i++){
+    for(int i = 4; i < 8; i++){
         cout<< a->toAttack().at(i)->getTerritoryName() << endl;
     }
 
     // testing issueOrder method
     a->issueOrder();
 
+    // deleting created objects to prevent 
+    // memory leaks
+    delete(a);
+    delete(deckOfCards);
+    // avoid dangling pointers
+    a = NULL;
+    deckOfCards = NULL;
 }
